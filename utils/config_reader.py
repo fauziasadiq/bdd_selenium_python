d31dd55817cca9efb7e1691c505408a3f4ffffe1
fauziasadiq <72159566+fauziasadiq@users.noreply.github.com>
2026-08-12 14:36:51 -0400

    Fix GitHub Actions setup in config_reader file

--- utils/config_reader.py
+++ utils/config_reader.py
@@ -1,6 +1,12 @@
-
+import os
 import yaml
 
+
 def load_config():
-    with open("config/config.yaml") as file:
-        return yaml.safe_load(file)
+    with open("config/config.yaml", "r") as file:
+        config = yaml.safe_load(file)
+
+    if os.getenv("CI") == "true":
+        config["headless"] = True
+
+    return config
